e156765605d5114e10ecf0d8cd71ed7856708508
lcfend <leecoombes@mail.de>
2025-02-03 04:20:54 -0500

    login/signup

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import React, { useEffect } from 'react';
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Navbar from './Components/Navbar/Navbar';
 import Landing_Page from './Components/Landing_Page/Landing_Page';
+import Login from "./Components/Login/Login";
+import SignUp from "./Components/Sign_Up/Sign_Up";
 
 function App() {
   return (
@@ -10,6 +12,9 @@ function App() {
           <Navbar/>
           <Routes>
           <Route path="/" element={<Landing_Page/>}/>
+          <Route path="/Login" element={<Login/>}/>
+          <Route path="/SignUp" element={<SignUp/>}/>
+
           </Routes>
         </BrowserRouter>
     </div>
